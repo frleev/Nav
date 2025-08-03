1fe23d9a230087190b2bdcf1e611cdcf068bf929
frleev <frlee2025@gmail.com>
2025-08-03 16:42:36 +0800

    Update search-bar.tsx

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -122,7 +122,6 @@ export function SearchBar({ navigationData, onSearch, searchResults, searchQuery
       </Command>
 
       <kbd className="pointer-events-none absolute right-2 top-2.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-xs text-muted-foreground opacity-100 sm:flex">
-        <span className="text-xs">⌘</span>K
       </kbd>
     </div>
   )
